21ad52df1485d1798a1edb58d34c11de7aed8913
boazbenmoshe <benmo@g.ariel.ac.il>
2020-11-09 10:37:12 +0200

    first version of PointTest - trivial tests

---
+++ src/PointTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+    private Point _oo = null;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        _oo = new Point(0,0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void get_x() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get_y() {
+    }
+}
